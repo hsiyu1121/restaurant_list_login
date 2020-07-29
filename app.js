96b11c1dfa704ef2068187d3f5958371b8eba607
hsiyu1121 <bluefish1121@gmail.com>
2020-07-29 16:04:39 +0800

    feat: add mongodb

--- app.js
+++ app.js
@@ -1,31 +1,63 @@
 const express = require("express");
 const app = express();
+const mongoose = require('mongoose')
 const exphbs = require("express-handlebars");
-const restaurantList = require("./restaurant.json");
+const bodyParser = require('body-parser')
+// const restaurantList = require("./restaurant.json");
 const port = 3000;
 
-app.engine('handlebars', exphbs({defaultLayout:'main'}))
-app.set('view engine', 'handlebars')
+app.engine('hbs', exphbs({defaultLayout:'main', extname: '.hbs'}))
+app.set('view engine', 'hbs')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true}))
+
+mongoose.connect("mongodb://localhost/restautant", {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+})
+
+
+const db = mongoose.connection
+
+db.on("error", () => {
+  console.log("mongodb error!")
+})
+db.once("open", () => {
+  console.log("mongodb connected!")
+})
 
 // index 位置
-app.get('/', (req, res)=>{
-  res.render('index', {restaurant:restaurantList.results})
+app.get('/', (req,res) => {
+  res.render('index')
 })
 
-app.get('/restaurants/:restaurant_id', (req, res)=>{
-  const restaurant = restaurantList.results.find(restaurant=> restaurant.id.toString() === req.params.restaurant_id)
-  res.render('show', {restaurant:restaurant})
-})
+// app.get('/', (req, res) => {
+//   res.render('index', {restaurant:restaurantList.results})
+// })
 
-app.get('/search', (req, res)=>{
-  console.log(req.query)
-  const keyword = req.query.keyword
-  const restaurant = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
-  })
-  res.render('index', {restaurant:restaurant})
-})
+// app.get('/restaurants/:restaurant_id', (req, res)=>{
+//   const restaurant = restaurantList.results.find(restaurant => 
+//   restaurant.id.toString() === req.params.restaurant_id)
+//   res.render('show', {restaurant:restaurant})
+// })
+
+// app.get('/search', (req, res)=>{
+//   console.log(req.query)
+//   const keyword = req.query.keyword
+//   const restaurant_name = restaurantList.results.filter((restaurant) => {
+//     return restaurant.name.toLowerCase().includes(keyword.toLowerCase())
+//   })
+//   const restaurant_location = restaurantList.results.filter(restaurant => {
+//     return restaurant.location.toLowerCase().includes(keyword.toLowerCase())
+//   })
+
+//   const result = restaurant_name.concat(
+//     restaurant_location.filter((e)=>{
+//     return restaurant_name.indexOf(e) === -1
+//   })
+//   )
+//   res.render('index', {restaurant:result})
+// })
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`);
