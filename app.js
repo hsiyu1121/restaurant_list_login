0d5526b5490bcd0c0743f2d8eaa75806ffb970d8
hsiyu1121 <bluefish1121@gmail.com>
2020-08-03 17:47:59 +0800

    refactor: modify search function

--- app.js
+++ app.js
@@ -17,75 +17,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
 app.use(routes)
 
-
-
-// index 位置
-// app.get("/", (req, res) => {
-//   Restaurant.find()
-//     .lean()
-//     .then(restaurants => res.render("index", { restaurants }))
-//     .catch( error => console.log(error))
-// });
-
-// app.get('/restaurants/new', (req, res) => {
-//   return res.render('new')
-// })
-
-// app.post('/create', (req, res) => { 
-//   return Restaurant.create( req.body )
-//     .then( () => res.redirect('/'))
-//     .catch(error => console.log(error))
-// })
-
-// app.get('/restaurants/:id', (req, res) => {
-//   const id = req.params.id
-//   return Restaurant.findById(id)
-//     .lean()
-//     .then((restaurants) => res.render('show', { restaurants }))
-//     .catch(error => console.log(error))
-// })
-
-// app.get('/restaurants/:id/edit', (req, res) => {
-//   const id = req.params.id
-//   return Restaurant.findById(id)
-//     .lean()
-//     .then((restaurant) => res.render('edit', {restaurant}))
-//     .catch(error => console.log(error))
-// })
-
-// app.put('/restaurants/:id', (req, res) => {
-//   const id = req.params.id
-//   return Restaurant.findById(id)
-//     .then(restaurant => {
-//      Object.assign(restaurant, req.body)
-//       return restaurant.save()
-//     })
-//    .then( () => res.redirect(`/restaurants/${id}`))
-//    .catch(error => console.log(error))  
-// })  
-
-// app.delete('/restaurants/:id', (req,res) => {
-//   const id = req.params.id
-//   return Restaurant.findById(id)
-//     .then(restaurant => restaurant.remove())
-//     .then(() => res.redirect('/'))
-//     .catch(error => console.log(error))
-// })
-
-app.get('/search', (req, res)=>{
-  console.log(req.query)
-  const keyword = req.query.keyword
-  
-  Restaurant.find()
-    .or([{name:{$regex:keyword, $options:'i'}},
-        {name_en:{$regex:keyword, $options:'i'}},
-        {location:{$regex:keyword, $options:'i'}},
-        {category:{$regex:keyword, $options:'i'}}
-    ])
-    .lean()
-    .then(restaurants => res.render('index', { restaurants }))
-    .catch(error => console.log(error))
+app.get('/sort', (req, res)=>{
+  console.log(req)
 })
+
+
+
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`);
---
+++ routes/modules/search.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+
+const Restaurant = require('../../models/restaurant')
+
+
+router.get('/', (req, res) => {
+  const keyword = req.query.keyword.toLowerCase()
+
+  Restaurant.find()
+    .lean()
+    .then((restaurants) => {
+      return restaurants.filter(restaurant => 
+        restaurant.name.toLowerCase().includes(keyword) || 
+        restaurant.category.toLowerCase().includes(keyword) || 
+        restaurant.location.toLowerCase().includes(keyword)
+      )
+    })
+    .then(restaurants => res.render('index', {restaurants, keyword}))
+    .catch(error => console.log(error))
+})
+
+
+module.exports = router
